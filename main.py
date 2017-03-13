982d7d01b1683e9f2d0c68aa13b70bc7fe3fbd09
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 02:28:14 -0700

    Added Hello World

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from microsoftbotframework.msbot import MsBot
 from tasks import *
 
 bot = MsBot()
-#bot.add_process(respond_to_conversation_update)
+bot.add_process(hello_world)
 bot.add_process(chat_bot_respond)
 bot.run()
