bdd4bb0c63809e1b1fe0161e94b9c935a80cba9d
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 02:24:22 -0700

    Added auto-port assignment

--- microsoftbotframework/msbot.py
+++ microsoftbotframework/msbot.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from microsoftbotframework.helpers import ConfigSectionMap
 from celery.local import PromiseProxy
+import os
 
 class MsBot:
     def __init__(self):
@@ -25,7 +26,8 @@ class MsBot:
         self.processes.append(process)
 
     def run(self):
-        self.app.run()
+        port = int(os.environ.get("PORT", 5000))
+        self.app.run(host='0.0.0.0', port=port)
 
     def auto_load(self):
         #TODO: Autoload packages in tasks.py
