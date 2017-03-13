4307042c6ffff22ccea791216d5e34b8092cf2f7
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 07:15:43 -0700

    Added microsoft

--- microsoftbotframework/response.py
+++ microsoftbotframework/response.py
@@ -48,7 +48,8 @@ class Response:
         conversation_id = self['conversation']["id"] if conversation is None else conversation['id']
         replyToId = self['id'] if replyToId is None else replyToId
 
-        responseURL = "{}v3/conversations/{}/activities/{}".format(self["serviceUrl"], conversation_id, replyToId)
+        #responseURL = "{}v3/conversations/{}/activities/{}".format(self["serviceUrl"], conversation_id, replyToId)
+        responseURL = "{}v3/conversations/{}/activities".format(self["serviceUrl"], conversation_id, replyToId)
 
         response_json = {
             "from": self["recipient"] if fromInfo is None else fromInfo,
@@ -57,7 +58,7 @@ class Response:
             "conversation": self['conversation'] if conversation is None else conversation,
             "recipient": self["from"] if recipient is None else recipient,
             "text": message,
-            "replyToId": replyToId
+ #           "replyToId": replyToId
         }
 
         print('responseURL:', responseURL)
