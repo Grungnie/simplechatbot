87c4a888fd973ba9a506d6fc4b6b211388f2eac6
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 03:46:44 -0700

    Added db connection

--- tasks.py
+++ tasks.py
@@ -24,7 +24,9 @@ def chat_bot_respond(message):
         chatbot = ChatBot(
             'Smart Harry',
             trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-#            storage_adapter="chatterbot.storage.MongoDatabaseAdapter"
+            storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
+            database='heroku_503f2tll',
+            database_uri='mongodb://createuser:MatthewBrown@ds129600.mlab.com:29600/heroku_503f2tll'
         )
 
         response = Response(message)
