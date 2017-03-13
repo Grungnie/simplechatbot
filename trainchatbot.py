8674bac0113f706f341e97318891e333240a4e47
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 03:32:40 -0700

    Added db connection

--- trainchatbot.py
+++ trainchatbot.py
@@ -5,7 +5,7 @@ chatbot = ChatBot(
     'Smart Harry',
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
     storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
-    database_uri='mongodb://skippy:MatthewBrown@ds129600.mlab.com:29600/heroku_503f2tll'
+    database_uri='mongodb://createuser:MatthewBrown@ds129600.mlab.com:29600/heroku_503f2tll'
 )
 
 # Train based on the english corpus
