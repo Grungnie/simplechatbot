1b181744c77fe0d6a4224856e29a3a474443d871
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 04:40:25 -0700

    Added Celery

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import microsoftbotframework.runcelery
-from microsoftbotframework.msbot import MsBot
-from tasks import *
-import subprocess
+if __name__ == "__main__":
+    import microsoftbotframework.runcelery
+    from microsoftbotframework.msbot import MsBot
+    from tasks import *
+    import subprocess
 
-import trainchatbot
+    import trainchatbot
 
-# Start celery workers
-subprocess.Popen('celery -A microsoftbotframework.runcelery.celery --concurrency=2 worker', shell=True)
+    # Start celery workers
+    subprocess.Popen('celery -A microsoftbotframework.runcelery.celery --concurrency=2 worker', shell=True)
 
-bot = MsBot()
-#bot.add_process(hello_world)
-bot.add_process(chat_bot_respond)
-bot.run()
+    bot = MsBot()
+    #bot.add_process(hello_world)
+    bot.add_process(chat_bot_respond)
+    bot.run()
