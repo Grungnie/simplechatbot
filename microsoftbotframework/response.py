56ff0248006635086b23a123c3cd3c033d54da94
Matthew Brown <mbrown1508@outlook.com>
2017-03-13 07:58:31 -0700

    Added microsoft

--- microsoftbotframework/response.py
+++ microsoftbotframework/response.py
@@ -49,12 +49,12 @@ class Response:
         conversation_id = self['conversation']["id"] if conversation is None else conversation['id']
         replyToId = self['id'] if replyToId is None else replyToId
 
-        self['serviceUrl'] = 'https://webchat.botframework.com/'
-        responseURL = "{}v3/conversations/{}/activities/{}".format(self["serviceUrl"], conversation_id, replyToId)
-        #responseURL = "{}v3/conversations/{}/activities".format(self["serviceUrl"], conversation_id, replyToId)
+        #self['serviceUrl'] = 'https://webchat.botframework.com/'
+        #responseURL = "{}v3/conversations/{}/activities/{}".format(self["serviceUrl"], conversation_id, replyToId)
+        responseURL = "{}v3/conversations/{}/activities".format(self["serviceUrl"], conversation_id, replyToId)
 
         response_json = {
-            "from": {'id': self.config['app_client_id'], "name": "SmartHarry"} if fromInfo is None else fromInfo,
+            "from": self["recipient"] if fromInfo is None else fromInfo,
             "type": 'message' if type is None else type,
             "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%zZ"),
             "conversation": self['conversation'] if conversation is None else conversation,
