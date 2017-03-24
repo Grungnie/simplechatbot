9aa10eac95e52b270d664728731baa6829ce53c8
Matthew Brown <mbrown1508@outlook.com>
2017-03-24 05:53:10 -0700

    Cast port to int

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     # Train the bot if it hasn't already
     import trainchatbot
 
-    bot = MsBot(port=os.environ['PORT'], debug=True)
+    bot = MsBot(port=int(os.environ['PORT']), debug=True)
     bot.add_process(chat_bot_respond)
     bot.run()
