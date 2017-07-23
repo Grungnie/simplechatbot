eb3230af13019deba17aa94bf1b704cd2f6b8fc3
Matthew Brown <mbrown1508@outlook.com>
2017-07-23 17:35:30 +1000

    Updated to v3 and removed additional inteligence that was attempted

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-if __name__ == "__main__":
-    from microsoftbotframework import MsBot
-    from tasks import *
-    import os
+from tasks import chat_bot_respond
+from microsoftbotframework import MsBot
+import logging
 
-    # Train the bot if it hasn't already
-    import trainchatbot
 
-    bot = MsBot(port=int(os.environ['PORT']), debug=True)
-    bot.add_process(chat_bot_respond_async)
+log_format = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+formatter = logging.Formatter(log_format)
+
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+streamHandler.setFormatter(formatter)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=log_format)
+
+bot = MsBot()
+bot.add_process(chat_bot_respond)
+
+log = bot.logger
+log.setLevel(logging.INFO)
+log.addHandler(streamHandler)
+
+if __name__ == '__main__':
     bot.run()
--- tasks.py
+++ tasks.py
@@ -1,26 +1,66 @@
 from microsoftbotframework import ReplyToActivity
 import celery
 from chatterbot import ChatBot
+#from chatterbot.comparisons import jaccard_similarity, synset_distance, sentiment_comparison
 import os
+import subprocess
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 @celery.task()
-def chat_bot_respond_async(message):
-    chat_bot_respond(message)
-
-
 def chat_bot_respond(message):
-    if message["type"]=="message":
-        chatbot = ChatBot(
-            'Smart Harry',
-            trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-            storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
-            database=os.environ['MONGO_DATABASE_NAME'],
-            database_uri=os.environ['MONGO_DATABASE_URI']
-        )
+    if message["type"] == "message":
+        chatbot = get_chatbot()
+        if message['text'] == '!train_smart_harry':
+            ReplyToActivity(fill=message,
+                            text='I am learning!').send()
+            train_smart_harry(chatbot)
+            ReplyToActivity(fill=message,
+                            text='I think I got smarter.').send()
+        else:
+            message_response = chatbot.get_response(message["text"]).text
 
-        message_response = chatbot.get_response(message["text"]).text
+            ReplyToActivity(fill=message,
+                            text=message_response).send()
 
-        ReplyToActivity(fill=message,
-                        text=message_response).send()
 
+def train_smart_harry(chatbot):
+    # Chatterbot Corpus
+    process = subprocess.Popen("git clone https://github.com/gunthercox/chatterbot-corpus.git", shell=True)
+    process.wait()
+    chatbot.train('chatterbot-corpus.chatterbot_corpus.data.english')
+    subprocess.Popen("rm -R chatterbot-corpus", shell=True)
+
+    # Ubuntu Corpus
+    #chatbot.train()
+
+def get_chatbot():
+    return ChatBot(
+        'Smart Harry',
+        trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+        storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
+        database=os.environ['MONGO_DATABASE_NAME'],
+        database_uri=os.environ['MONGO_DATABASE_URI']
+    )
+
+
+# def get_chatbot():
+#     return ChatBot(
+#         'Smart Harry',
+#         storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
+#         #database=os.environ['MONGO_DATABASE_NAME'],
+#         database='test_twitter',
+#         database_uri=os.environ['MONGO_DATABASE_URI'],
+#         logic_adapters=[
+#             'chatterbot.logic.BestMatch',
+#         ],
+#         filters=["chatterbot.filters.RepetitiveResponseFilter"],
+#         twitter_consumer_key=os.getenv("CONSUMER_KEY"),
+#         twitter_consumer_secret=os.getenv("CONSUMER_SECRET"),
+#         twitter_access_token_key=os.getenv("ACCESS_TOKEN"),
+#         twitter_access_token_secret=os.getenv("ACCESS_TOKEN_SECRET"),
+#         trainer="chatterbot.trainers.TwitterTrainer",
+#         logger=logger,
+#     )
